4773a70687847dcbd1b1b8dd7438b8f17eebe6f1
Luke Barlow <luke.barlow@gmail.com>
2017-10-05 15:14:40 +0100

    added build script to basic example

--- examples/basic/webpack.config.js
+++ examples/basic/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'y-plain-state-basic-example.js',
     publicPath: '/js/'
   },
   module: {
